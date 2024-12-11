fc9523d359af3697eb81f4ff5a3438a04a9d1a06
atzr95 <atzr95@gmail.com>
2024-12-11 22:29:44 +0800

    add List extensions

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -201,6 +201,100 @@ Widget gap(double gap, {double? crossGap}) {
   );
 }
 
+extension StyledList<E> on List<Widget> {
+  Widget toColumn({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    Widget? separator,
+  }) =>
+      Column(
+        key: key,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        textBaseline: textBaseline,
+        children: separator != null && this.isNotEmpty
+            ? (this.expand((child) => [child, separator]).toList()
+              ..removeLast())
+            : this,
+      );
+
+  Widget toRow({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    Widget? separator,
+  }) =>
+      Row(
+        key: key,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        textBaseline: textBaseline,
+        children: separator != null && this.isNotEmpty
+            ? (this.expand((child) => [child, separator]).toList()
+              ..removeLast())
+            : this,
+      );
+
+  Widget toStack({
+    Key? key,
+    AlignmentGeometry alignment = AlignmentDirectional.topStart,
+    TextDirection? textDirection,
+    StackFit fit = StackFit.loose,
+    Clip clipBehavior = Clip.hardEdge,
+    List<Widget> children = const <Widget>[],
+  }) =>
+      Stack(
+        key: key,
+        alignment: alignment,
+        textDirection: textDirection,
+        fit: fit,
+        clipBehavior: clipBehavior,
+        children: this,
+      );
+
+  Widget toWrap({
+    Key? key,
+    Axis direction = Axis.horizontal,
+    WrapAlignment alignment = WrapAlignment.start,
+    double spacing = 0.0,
+    WrapAlignment runAlignment = WrapAlignment.start,
+    double runSpacing = 0.0,
+    WrapCrossAlignment crossAxisAlignment = WrapCrossAlignment.start,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    Clip clipBehavior = Clip.none,
+    List<Widget> children = const <Widget>[],
+  }) =>
+      Wrap(
+        key: key,
+        direction: direction,
+        alignment: alignment,
+        spacing: spacing,
+        runAlignment: runAlignment,
+        runSpacing: runSpacing,
+        crossAxisAlignment: crossAxisAlignment,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        clipBehavior: clipBehavior,
+        children: this,
+      );
+}
+
 extension Joinable<T extends Widget> on List<T> {
   List<T> joinSeparator(T separator) {
     List<T> result = [];
@@ -392,6 +486,16 @@ extension WidgetExtension on Widget {
       child: this,
     );
   }
+
+  Widget flexible({
+    int flex = 1,
+    FlexFit fit = FlexFit.loose,
+  }) =>
+      Flexible(
+        child: this,
+        flex: flex,
+        fit: fit,
+      );
 
   Widget withOpacity(double opacity) {
     return Opacity(
