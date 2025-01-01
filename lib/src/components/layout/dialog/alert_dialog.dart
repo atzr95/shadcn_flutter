58b159765aa93af379ab8689b03adf394f90a29e
Anthony <atzr95@gmail.com>
2025-01-01 21:24:52 +0800

    imporve alertdialog

--- lib/src/components/layout/dialog/alert_dialog.dart
+++ lib/src/components/layout/dialog/alert_dialog.dart
@@ -11,6 +11,8 @@ class AlertDialog extends StatefulWidget {
   final Color? barrierColor;
   final EdgeInsetsGeometry? padding;
   final bool isFullscreen;
+  final bool adjustForKeyboard;
+  final bool dismissKeyboardOnTapOutside;
 
   const AlertDialog({
     super.key,
@@ -23,6 +25,8 @@ class AlertDialog extends StatefulWidget {
     this.surfaceOpacity,
     this.barrierColor,
     this.padding,
+    this.adjustForKeyboard = false,
+    this.dismissKeyboardOnTapOutside = true,
   }) : isFullscreen = false;
 
   const AlertDialog.fullscreen({
@@ -36,6 +40,8 @@ class AlertDialog extends StatefulWidget {
     this.surfaceOpacity,
     this.barrierColor,
     this.padding,
+    this.adjustForKeyboard = true,
+    this.dismissKeyboardOnTapOutside = true,
   }) : isFullscreen = true;
 
   @override
@@ -43,80 +49,98 @@ class AlertDialog extends StatefulWidget {
 }
 
 class _AlertDialogState extends State<AlertDialog> {
+  void _unfocus() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     var themeData = Theme.of(context);
     var scaling = themeData.scaling;
+    var mediaQuery = MediaQuery.of(context);
+
     return ModalContainer(
       borderRadius:
           widget.isFullscreen ? BorderRadius.zero : themeData.borderRadiusXxl,
       barrierColor: widget.barrierColor ?? Colors.black.withOpacity(0.8),
       surfaceClip: ModalContainer.shouldClipSurface(
           widget.surfaceOpacity ?? themeData.surfaceOpacity),
-      child: OutlinedContainer(
-        backgroundColor: themeData.colorScheme.popover,
-        borderRadius:
-            widget.isFullscreen ? BorderRadius.zero : themeData.borderRadiusXxl,
-        borderWidth: widget.isFullscreen ? 0 : 1 * scaling,
-        borderColor: themeData.colorScheme.muted,
-        padding: widget.isFullscreen
-            ? EdgeInsets.zero
-            : widget.padding ?? EdgeInsets.all(24 * scaling),
-        surfaceBlur: widget.surfaceBlur ?? themeData.surfaceBlur,
-        surfaceOpacity: widget.surfaceOpacity ?? themeData.surfaceOpacity,
-        child: Column(
-          mainAxisSize:
-              widget.isFullscreen ? MainAxisSize.max : MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Flexible(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  if (widget.leading != null)
-                    widget.leading!.iconXLarge().iconMutedForeground(),
-                  if (widget.title != null || widget.content != null)
-                    Flexible(
-                      child: Column(
-                        mainAxisSize: widget.isFullscreen
-                            ? MainAxisSize.max
-                            : MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (widget.title != null)
-                            widget.title!.large().semiBold(),
-                          if (widget.content != null)
-                            Flexible(
-                              child: widget.content!.small().muted(),
-                            ),
-                        ],
-                      ).gap(8 * scaling),
-                    ),
-                  if (widget.trailing != null)
-                    widget.trailing!.iconXLarge().iconMutedForeground(),
-                ],
-              ).gap(16 * scaling),
-            ),
-            if (widget.actions != null && widget.actions!.isNotEmpty)
-              Padding(
-                padding: widget.isFullscreen
-                    ? EdgeInsets.only(
-                        bottom: MediaQuery.of(context).padding.bottom +
-                            16 * scaling,
-                        right: 16 * scaling,
-                        left: 16 * scaling,
-                      )
-                    : EdgeInsets.zero,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: join(widget.actions!, SizedBox(width: 8 * scaling))
-                      .toList(),
+      child: GestureDetector(
+        onTap: widget.dismissKeyboardOnTapOutside ? _unfocus : null,
+        behavior: HitTestBehavior.translucent,
+        child: AnimatedPadding(
+          padding: widget.adjustForKeyboard
+              ? EdgeInsets.only(bottom: mediaQuery.viewInsets.bottom)
+              : EdgeInsets.zero,
+          duration: const Duration(milliseconds: 100),
+          child: OutlinedContainer(
+            backgroundColor: themeData.colorScheme.popover,
+            borderRadius: widget.isFullscreen
+                ? BorderRadius.zero
+                : themeData.borderRadiusXxl,
+            borderWidth: widget.isFullscreen ? 0 : 1 * scaling,
+            borderColor: themeData.colorScheme.muted,
+            padding: widget.isFullscreen
+                ? EdgeInsets.zero
+                : widget.padding ?? EdgeInsets.all(24 * scaling),
+            surfaceBlur: widget.surfaceBlur ?? themeData.surfaceBlur,
+            surfaceOpacity: widget.surfaceOpacity ?? themeData.surfaceOpacity,
+            child: Column(
+              mainAxisSize:
+                  widget.isFullscreen ? MainAxisSize.max : MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Flexible(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      if (widget.leading != null)
+                        widget.leading!.iconXLarge().iconMutedForeground(),
+                      if (widget.title != null || widget.content != null)
+                        Flexible(
+                          child: Column(
+                            mainAxisSize: widget.isFullscreen
+                                ? MainAxisSize.max
+                                : MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (widget.title != null)
+                                widget.title!.large().semiBold(),
+                              if (widget.content != null)
+                                Flexible(
+                                  child: widget.content!.small().muted(),
+                                ),
+                            ],
+                          ).gap(8 * scaling),
+                        ),
+                      if (widget.trailing != null)
+                        widget.trailing!.iconXLarge().iconMutedForeground(),
+                    ],
+                  ).gap(16 * scaling),
                 ),
-              ),
-          ],
-        ).gap(16 * scaling),
+                if (widget.actions != null && widget.actions!.isNotEmpty)
+                  Padding(
+                    padding: widget.isFullscreen
+                        ? EdgeInsets.only(
+                            bottom: MediaQuery.of(context).padding.bottom +
+                                16 * scaling,
+                            right: 16 * scaling,
+                            left: 16 * scaling,
+                          )
+                        : EdgeInsets.zero,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children:
+                          join(widget.actions!, SizedBox(width: 8 * scaling))
+                              .toList(),
+                    ),
+                  ),
+              ],
+            ).gap(16 * scaling),
+          ),
+        ),
       ),
     );
   }
